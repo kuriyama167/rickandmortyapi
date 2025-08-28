e85f9337b6ac713589bad780382f441b814c34f4
kuriyama167 <velasquezclara167@gmail.com>
2025-08-27 22:03:25 -0600

    second

--- PythonReview/code.py
+++ PythonReview/code.py
@@ -2,9 +2,24 @@ name = "bob"
 greeting = "Hello, {}"
 with_name= greeting.format(name)
 
-print(with_name)
+##print(with_name)
 
 longer_phrase = "Hello, {}. Today is {}."
 
 formatted = longer_phrase.format("Rolf", "Monday")
-print(formatted)
+##print(formatted)
+
+#name = input("Enter your name: ")
+##print(name)
+
+#size_input = input("How bgif is ypur house (in feet): ")
+#square_feet = int(size_input)
+#square_metres = square_feet / 10.8
+##print(square_metres)
+##print(f"{square_feet} square feet is {square_metres:.2f} square metres.")
+
+user_age = int(input("Enter your age: "))
+
+months = user_age * 12
+print(f"Your Age, {user_age}, is equal to {months} months ")
+
