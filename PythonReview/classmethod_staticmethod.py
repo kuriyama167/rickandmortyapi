9720013b205a864c101ace068d2641bef0ddfd8e
kuriyama167 <velasquezclara167@gmail.com>
2026-01-29 16:50:03 -0600

    Classmethod and statiis exercises

---
+++ PythonReview/classmethod_staticmethod.py
@@ -0,0 +1,25 @@
+class Book:
+    TYPES = ("hardcover", "paperback", "ebook")
+
+    def __init__(self, name, book_type, weight):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self):
+        return f"<Book('{self.name}', '{self.book_type}', {self.weight})g>"
+    
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return Book(name, cls.TYPES[0], page_weight + 100)
+    
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return Book(name, cls.TYPES[1], page_weight + 50)
+
+book = Book.hardcover("Harry Potter",  1.5)   
+
+light = Book.paperback("Ring of the powers", 1.5)   
+
+print(book)
+print(light)
